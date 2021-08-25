b67137aea406f3353e9b642045024e50f16b8df4
Preethi Boina <pbachirbp@gmail.com>
2021-08-25 09:00:54 -0400

    Final v2 commit

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Join the CrazyBlog newsletter to receive our best stories
         </p>
         <p className='footer-subscription-text'>
           You can unsubscribe at any time.
@@ -29,29 +29,22 @@ function Footer() {
           <div class='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/signup'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+          
           </div>
           <div class='footer-link-items'>
-            <h2>Contact Us</h2>
+            <h2>ContactUs</h2>
             <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+           
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>Videos</h2>
             <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+          
           </div>
           <div class='footer-link-items'>
-            <h2>Social Media</h2>
+            <h2>SocialMedia</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
