3f6453c54788e5f341585003da46c91de99ee776
Preethi Boina <pbachirbp@gmail.com>
2021-08-30 11:24:53 -0400

    Deleted extra folder

--- connecting/Server/controllers/blogController.js
+++
@@ -1,9 +0,0 @@
-const blogModel = require('../models/blog');
-
-
-function getAllData(req, res) {
-    const blogData = blogModel.getAllData();
-    res.status(200).json(blogData);
-  }
-  
-  module.exports = { getAllData };
--- connecting/Server/seeds/04_comment.js
+++
@@ -1,9 +0,0 @@
-const commentData = require("./seeds_data/comments")
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('comments').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('comments').insert(commentData);
-    });
-};
