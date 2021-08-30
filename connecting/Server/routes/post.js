8566fd915d36ad84884c09e946080f766a97c008
Preethi Boina <pbachirbp@gmail.com>
2021-08-30 11:22:57 -0400

    Deleted extra folder

--- connecting/Server/routes/post.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = require("express").Router();
-const blogModel = require("../models/blog");
-const userModel = require("../models/user");
-const postModel = require("../models/post");
-
-module.exports = router;
--- connecting/Server/seeds/02_blog.js
+++
@@ -1,9 +0,0 @@
-const blogData = require("./seeds_data/blogs")
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('blogs').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('blogs').insert(blogData);
-    });
-};
